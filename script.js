6281cd60fa3ddd842e21e92767e8f5fc42e8bb17
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-03-02 23:26:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let message = document.createElement("p"); 
-    message.textContent = "DOM load success";  
-    document.body.appendChild(message);       
+document.addEventListener("DOMContentLoaded", function() {
+    const textNode = document.createTextNode("DOM load success");
+    document.body.appendChild(textNode);
 });
