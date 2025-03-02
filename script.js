bd609d79b235102f3f580e149ef2d231af9faf91
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-03-02 23:20:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", () => {
-	document.body.innerHTML = "<h2>DOM load success</h2>";
+	console.log("DOM load success")
 })
